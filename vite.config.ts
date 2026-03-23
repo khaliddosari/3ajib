7b55b7867e5b6d25a475550f1f542fd1a3ae6d0e
Khalid <jc7rw9z7y9@privaterelay.appleid.com>
2026-03-24 01:29:05 +0300

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/3ajib/',
   server: {
     host: "::",
     port: 8080,
